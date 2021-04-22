d5d72ce381aa8fa360cd3c7d6dafeea6acba97e6
Maximous Black <maximousblk@gmail.com>
2021-04-22 20:50:06 +0000

    fix: add exports for `fnm`

--- .zsh_exports.zsh
+++ .zsh_exports.zsh
@@ -27,3 +27,7 @@ export STATUS
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+
+# fnm
+export PATH=/home/codespace/.fnm:$PATH
+eval "`fnm env`"
